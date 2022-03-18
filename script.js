556ea1c1468dc7fbc24baf52b14fca41449ad54a
Marion4000 <marion.marrot@gmail.com>
2022-03-18 11:12:24 +0100

    dernière maj

--- script.js
+++ script.js
@@ -19,29 +19,39 @@ aNav.forEach( a => {
         } }
         )
     })
-const btnHamburger = document.querySelector('#btnHamburger');
-const header = document.querySelector('header');
+// const btnHamburger = document.querySelector('#btnHamburger');
+// const header = document.querySelector('header');
+// const menuMobile = document.querySelector('.nav_mobile');
+
+
+// btnHamburger.addEventListener('click', function(){
+//     console.log('click hamburger');
+
+//     if(header.classList.contains('open')){ //Close hamburger menu
+//         header.classList.remove('open');
+//         menuMobile.classList.add('fade-out');
+//         menuMobile.classList.remove('fade-in');
+        
+        
+//     }
+//     else{ //Open hamburger menu
+//         header.classList.add('open'); 
+//         menuMobile.classList.add('fade-in');
+//         menuMobile.classList.remove('fade-out');
+// }
+// })
+
+const btnHamurger = document.querySelector('#btnHamburger');
 const menuMobile = document.querySelector('.nav_mobile');
-const overlay = document.querySelector('.overlay');
+const liens = document.querySelectorAll('.nav_mobile a');
 
-
-btnHamburger.addEventListener('click', function(){
-    console.log('click hamburger');
-
-    if(header.classList.contains('open')){ //Close hamburger menu
-        header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
-        menuMobile.classList.add('fade-out');
-        menuMobile.classList.remove('fade-in');
-        
-        
-    }
-    else{ //Open hamburger menu
-        header.classList.add('open'); 
-        overlay.classList.add('fade-in');
-        overlay.classList.remove('fade-out');
-        menuMobile.classList.add('fade-in');
-        menuMobile.classList.remove('fade-out');
-}
+btnHamurger.addEventListener('click', ()=> {
+    menuMobile.classList.toggle('nav-open');
 })
+
+liens.forEach(lien => {
+lien.addEventListener('click', ()=> {
+    menuMobile.classList.remove('nav-open');
+})
+})
+
